9f901734b418b0f6a1281d23eb0388465ba91245
Saravana priyan C <c.saravanapriyan@gmail.com>
2025-02-10 09:58:46 +0530

    Before styling the nav bar for smallscreens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,28 @@ const Navbar = () => (
       <li className='p__opensans'><a href="#awards">Awards</a></li>
       <li className='p__opensans'><a href="#contact">Contact</a></li>
     </ul>
+
+    <div className='app__navbar-login'>
+      <a href="#login" className='p__opensans'>Log In / Register</a>
+      <div/> {/* this div for the line between */}
+      <a href="/" className='p__opensans'>Book Table</a>
+    </div>
+
+    {/* now this is for small screeens and the icon menu div gihamburger is a icon */}
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
+
+      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => {}}/>
+        <ul className='app__navbar-smallscreen-links'>
+          <li className='p__opensans'><a href="#home">Home</a></li>
+          <li className='p__opensans'><a href="#about">About</a></li>
+          <li className='p__opensans'><a href="#menu">Menu</a></li>
+          <li className='p__opensans'><a href="#awards">Awards</a></li>
+          <li className='p__opensans'><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 
